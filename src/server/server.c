eb92fcdca69e0def451c0d0b9c03a0dac2fdfcd2
huangered <huangered@hotmail>
2020-07-16 16:05:29 +0800

    init

---
+++ src/server/server.c
@@ -0,0 +1,11 @@
+﻿// mmdb.cpp : Defines the entry point for the application.
+//
+
+#include "server/server.h"
+#include "access/test.h"
+
+int main(int argc, char* argv[])
+{
+	hello();
+	return 0;
+}
