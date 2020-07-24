712c3eb7f1169cf5d446b8263e8a2aea2048f338
huangered <huangered@hotmail>
2020-07-24 15:17:59 +0800

    add some headers

--- include/storage/bufmgr.h
+++ include/storage/bufmgr.h
@@ -1,6 +1,13 @@
 #ifndef _BUFMGR_H_
 #define _BUFMGR_H_
 
+#include "mimi.h"
+#include "access/rel.h"
+#include "storage/block.h"
 
+typedef uint16 Buffer;
+
+extern Buffer ReadBuffer(Relation rel, BlockNum blkno);
+extern void ReleaseBuffer(Buffer buffer);
 
 #endif // !_BUFMGR_H_
--- src/server/server.c
+++ src/server/server.c
@@ -3,10 +3,15 @@
 
 #include "server/server.h"
 #include "access/cbtree.h"
+#include "access/heap.h"
 #include "util/mctx.h"
+#include "storage/page.h"
 
 int main(int argc, char* argv[])
 {
 	btbuildempty(NULL);
+	Page page = palloc(8012);
+	PageInit(page);
+	heapbuildempty(NULL);
 	return 0;
 }
