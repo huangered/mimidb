72c0780e2b7efd9ddcb93a3ebfb3530dddb945b2
peter.huang <peter.huang@arm.com>
2020-07-27 15:42:15 +0800

    add gtest and fsm

---
+++ include/storage/fsm_internal.h
@@ -0,0 +1,16 @@
+#ifndef _fsm_internal_h_
+#define _fsm_internal_h_
+
+#include "access/rel.h"
+#include "storage/block.h"
+#include "bufmgr.h"
+
+struct FSMPageData {
+    int items[20];
+};
+
+typedef struct FSMPageData* FSMPage;
+
+BlockNum fsm_search_avail(Buffer buf, Size spaceNeed);
+void fsm_set_value(Buffer buf, BlockNum usedBlock, Size freeSpace);
+#endif // !_fsm_internal_h_
--- src/storage/freespace/indexfsm.c
+++ src/storage/freespace/indexfsm.c
@@ -1,5 +1,17 @@
 #include "storage/indexfsm.h"
+#include "storage/freespace.h"
 
 BlockNum GetFreeIndexPage(Relation rel) {
-    return 0;
+
+    BlockNum blkno = GetPageWithFreeSpace(rel, 8192 / 2);
+
+    if (blkno != INVALID_BLOCK) {
+        void RecordUsedIndexPage(rel, blkno);
+    }
+
+    return blkno;
+}
+
+void RecordUsedIndexPage(Relation rel, BlockNum usedBlock) {
+    RecordPageWithFreeSpace(rel, usedBlock, 0);
 }
