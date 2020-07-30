bcd51d722da883c0653c17ae90269cd9abceac10
huangered <huangered@icloud.com>
2020-07-30 16:00:52 +0800

    enhance hash tbl

--- test/test_hash.cpp
+++ test/test_hash.cpp
@@ -11,14 +11,31 @@ TEST(hash_test, basic)
     int* key = (int*) malloc(sizeof(int));
     *key = 1;
 
-    Hash* tbl = hash_create("test", string_hash, string_equal, 1);
-    tbl->list[1].next = (HashElem*)malloc(sizeof(HashElem));
-    tbl->list[1].next->next = NULL;
-    tbl->list[1].next->key = key;
-    tbl->list[1].next->value = malloc(sizeof(char));
-    *((char*)tbl->list[1].next->value) = '1';
+    Hash* tbl = hash_create("test", string_hash, string_equal, 1, 1);
+    tbl->list[0].next = (HashElem*)malloc(sizeof(HashElem));
+    tbl->list[0].next->next = NULL;
+    tbl->list[0].next->key = key;
+    tbl->list[0].next->value = malloc(sizeof(char));
+    *((char*)tbl->list[0].next->value) = '1';
     char* value = (char*) hash_search(tbl, Search, key);
 
     EXPECT_EQ(*value, '1');
+
+    hash_destroy(tbl);
+}
+
+TEST(hash_test, add)
+{
+    int* key = (int*)malloc(sizeof(int));
+    *key = 1;
+
+    Hash* tbl = hash_create("test", string_hash, string_equal, 1, 1);
+    
+    char* value = (char*)hash_search(tbl, Add, key);
+    *value = '2';
+    value = (char*)hash_search(tbl, Search, key);
+
+    EXPECT_EQ(*value, '2');
+
     hash_destroy(tbl);
 }
