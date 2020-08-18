31562b9de302bf7ca80ccb18483e6ef82c2d87ce
peter.huang <peter.huang@arm.com>
2020-08-18 09:56:12 +0800

    fix linux build warning

--- src/access/btree/btree.c
+++ src/access/btree/btree.c
@@ -7,7 +7,7 @@ void btbuildempty(Relation rel) {
     Page metap = palloc(BLKSZ);
     _bt_init_page(metap);
 
-    BTreeMetaData* metad = PageGetContent(metap);
+    BTreeMetaData* metad = (BTreeMetaData*)PageGetContent(metap);
     metad->root = P_NONE;
 
     // write to local file system
@@ -45,7 +45,7 @@ bool btgettuple(IndexScanDesc scan) {
 
 void _bt_init_page(Page page) {
     PageInit(page, BLKSZ, sizeof(BTreeSpecialData));
-    BTreeSpecial special = PageGetSpecial(page);
+    BTreeSpecial special = (BTreeSpecial)PageGetSpecial(page);
     special->level = 0;
     special->block_prev = INVALID_BLOCK;
     special->block_next = INVALID_BLOCK;
@@ -56,7 +56,7 @@ Buffer _bt_moveright(Relation rel, BTreeScan key, Buffer buf) {
     for (;;) {
         Page page = BufferGetPage(buf);
         PageHeader header = PageGetHeader(page);
-        BTreeSpecial special = PageGetSpecial(page);
+        BTreeSpecial special = (BTreeSpecial)PageGetSpecial(page);
 
         if (P_RIGHTMOST(special)) {
             break;
