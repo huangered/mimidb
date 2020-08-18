4d35461e3ce133a400a95acc6df3014d5ec8d35f
huangered <huangered@hotmail>
2020-08-18 15:37:31 +0800

    update buf.h

--- include/storage/buf.h
+++ include/storage/buf.h
@@ -1,6 +1,12 @@
 #ifndef _BUF_H_
 #define _BUF_H_
 
+/*
+zero is invalid
+*/
 typedef int Buffer;
+
+#define INVALID_BUFFER  0
+#define BufferIsInvalid(buf)    ((buf) == INVALID_BUFFER)
 
 #endif // !_BUFMGR_H_
