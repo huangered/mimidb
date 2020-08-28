7f14a060cec9fe68a6f6cf99cd2760168bafd58a
peter.huang <peter.huang@arm.com>
2020-08-28 16:34:03 +0800

    fix unittest

--- test/catalog/test_heap.cpp
+++ test/catalog/test_heap.cpp
@@ -5,6 +5,8 @@ _EXTERN_C
 #include "catalog/heap.h"
 #include "util/mctx.h"
 #include "storage/bufmgr.h"
+#include "storage/freespace.h"
+#include "catalog/mimi_code.h"
 
 _END_EXTERN_C
 
@@ -12,11 +14,21 @@ TEST(Catalog, heap) {
     BufferInit();
     RelationCacheInit();
 
+    Relation mrel = (Relation)palloc(sizeof(RelationData));
+    mrel->rnode = ClassRelationId;
+
+    for (int i = 0; i < 100; i++) {
+        RecordPageWithFreeSpace(mrel, i, BLKSZ);
+    }
+
+    pfree(mrel);
+
     TupleDesc tupdesc = (TupleDesc)palloc(sizeof(TupleDescData));
     tupdesc->natts = 0;
-    Relation rel = heap_create("test", 100, tupdesc);
+    Relation rel = heap_create("test", 10000, tupdesc);
     
     EXPECT_NE(rel, nullptr);
+
 
     pfree(rel);
     pfree(tupdesc);
