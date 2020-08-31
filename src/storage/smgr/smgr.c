9573e1cdc0b7ccd5726b5704fc96500bc6236671
huangered <huangered@icloud.com>
2020-08-31 17:29:44 +0800

    add file exist method

--- src/storage/smgr/smgr.c
+++ src/storage/smgr/smgr.c
@@ -1,7 +1,19 @@
 #include "storage/smgr.h"
+#include "access/rel.h"
+#include "access/relpath.h"
+#include "util/mctx.h"
+#include <Windows.h>
+#include "Shlwapi.h"
+#pragma comment(lib, "Shlwapi.lib")
 
 bool smgrexists(Relation rel, ForkNumber number) {
-    return false;
+    bool result = false;
+    char* path = GetRelPath(rel->rnode, number);
+
+    result = PathFileExists(path);
+
+    pfree(path);
+    return result;
 }
 
 BlockNumber smgrblocks(Relation rel, ForkNumber number) {
--- test/test_fsm.cpp
+++ test/test_fsm.cpp
@@ -3,13 +3,18 @@
 _EXTERN_C
 
 #include "storage/fsm_internal.h"
-
+#include "util/mctx.h"
+#include "storage/smgr.h"
 _END_EXTERN_C
 
 // test the basic usage in buff mgr.
 TEST(fsm, root)
 {
-    Buffer buf = fsm_readbuf(NULL, FSM_ROOT_ADDRESS, true);
+    RelationData rel;
+    rel.rnode = 2000;
+    rel.rd_smgr = (SmgrRelation)palloc(sizeof(SMgrRelationData));
+    rel.rd_smgr->smgr_fsm_nblocks = 0;
+    Buffer buf = fsm_readbuf(&rel, FSM_ROOT_ADDRESS, true);
     printf("buf %d", buf);
 }
 
