826ba2748bdd992494b6746967b418c953212e45
huangered <huangered@hotmail>
2020-08-31 17:02:08 +0800

    add fsm op

--- include/storage/fsm_internal.h
+++ include/storage/fsm_internal.h
@@ -11,6 +11,8 @@
 #define FSM_ROOT_LEVEL      (FSM_TREE_DEPTH - 1)
 #define FSM_BOTTOM_LEVEL    0
 
+#define FLEXIBLE_ARRAY_MEMBER
+
 typedef struct FsmAddress {
     int level;      /* level */
     int logpageno;  /* page number in the level */
@@ -20,12 +22,13 @@ typedef struct FsmAddress {
 static const FSMAddress FSM_ROOT_ADDRESS = { FSM_ROOT_LEVEL, 0 };
 
 struct FSMPageData {
-    int items[20];
+    int fp_next_slot;
+    int fp_nodes[FLEXIBLE_ARRAY_MEMBER];
 };
 
 typedef struct FSMPageData* FSMPage;
 
-extern Buffer fsm_readbuf(Relation rel, FSMAddress addr);
+extern Buffer fsm_readbuf(Relation rel, FSMAddress addr, bool extend);
 extern BlockNumber fsm_search_avail(Buffer buf, Size spaceNeed);
 extern void fsm_set_value(Buffer buf, BlockNumber usedBlock, Size freeSpace);
 extern void fsm_extend(Relation rel, BlockNumber blkno);
---
+++ src/storage/smgr/smgr.c
@@ -0,0 +1,9 @@
+#include "storage/smgr.h"
+
+bool smgrexists(Relation rel, ForkNumber number) {
+    return false;
+}
+
+BlockNumber smgrblocks(Relation rel, ForkNumber number) {
+    return 0;
+}
