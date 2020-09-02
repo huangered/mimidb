a1850d45d3a0793ea7e662cfcb75de322f16386c
huangered <huangered@icloud.com>
2020-09-02 15:18:16 +0800

    set the invalid buffer to 1

--- include/storage/buf.h
+++ include/storage/buf.h
@@ -6,7 +6,8 @@ zero is invalid
 */
 typedef int Buffer;
 
-#define INVALID_BUFFER  -1
+#define INVALID_BUFFER          0
 #define BufferIsInvalid(buf)    ((buf) == INVALID_BUFFER)
+#define BufferIsValid(buf)      ((buf) != INVALID_BUFFER)
 
 #endif // !_BUFMGR_H_
--- test/test_buff_mgr.cpp
+++ test/test_buff_mgr.cpp
@@ -13,14 +13,14 @@ TEST(hash_buff_mgr, basic)
 
     Buffer buf_id = ReadBuffer(rel, ForkNumber::MAIN_FORKNUMBER, 0);
 
-    EXPECT_EQ(buf_id, 0);
+    EXPECT_EQ(buf_id, 1);
 
     buf_id = ReadBuffer(rel, ForkNumber::MAIN_FORKNUMBER, 0);
 
-    EXPECT_EQ(buf_id, 0);
+    EXPECT_EQ(buf_id, 1);
 
     Buffer buf_id_2 = ReadBuffer(rel, ForkNumber::MAIN_FORKNUMBER, 1);
-    EXPECT_EQ(buf_id_2, 1);
+    EXPECT_EQ(buf_id_2, 2);
 
     ReleaseBuffer(buf_id);
     ReleaseBuffer(buf_id);
