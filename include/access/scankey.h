dd1a01bcd049141607051714c8ab3a6b556cc81d
huangered <huangered@icloud.com>
2020-09-09 13:40:28 +0800

    add fmgr component

--- include/access/scankey.h
+++ include/access/scankey.h
@@ -3,32 +3,16 @@
 
 #include "mimi.h"
 #include "access/strategynumber.h"
-
-
-typedef int (*cmp1)(Datum l, Datum r);
+#include "util/fmgr.h"
 
 typedef struct ScanKeyData {
     Datum sk_data;
-    cmp1 sk_comp;
+    FmgrInfo sk_func;
     StrategyNumber sk_number;
+
 } ScanKeyData;
 
 typedef ScanKeyData* ScanKey;
-
-static int int_cmp(Datum l, Datum r) {
-    int i = (int)l;
-    int j = (int)r;
-
-    if (i == j) {
-        return 0;
-    }
-    else if (i > j) {
-        return 1;
-    }
-    else {
-        return -1;
-    }
-}
 
 #endif // !_scankey_h_
 
--- src/access/btree/btutils.c
+++ src/access/btree/btutils.c
@@ -1,17 +1,31 @@
 #include "access/btree.h"
 #include "util/mctx.h"
 
+/*
+make a scan key
+*/
 BTreeScan _bt_make_scankey(Relation rel, IndexTuple itup) {
+    int nkeys = 1;
+    ScanKey skey;
+
     BTreeScan key = palloc(sizeof(BTreeScanData));
     key->itup = itup;
     key->itemsz = sizeof(IndexTupleData);
     key->nextkey = false;
-    key->keysz = 1;
-    key->scankeys[0].sk_data = itup->key;
-    key->scankeys[0].sk_comp = int_cmp;
+    key->keysz = nkeys;
+
+    skey = key->scankeys;
+
+    for (int i = 0; i < nkeys; i++) {
+        skey[i].sk_data = itup->key;
+        fmgr_info(1, &skey[i].sk_func);
+    }
     return key;
 }
 
+/*
+make a index tuple
+*/
 IndexTuple _bt_make_tuple(int key, int ht_id) {
     IndexTuple tup = palloc(sizeof(IndexTupleData));
     tup->key = key;
@@ -20,6 +34,9 @@ IndexTuple _bt_make_tuple(int key, int ht_id) {
     return tup;
 }
 
+/*
+free the tree stack
+*/
 void _bt_freestack(BTStack stack) {
     while (stack != NULL) {
         BTStack parent = stack->parent;
