419218604b23719ef56f3e98ac0589842c4a3611
huangered <huangered@icloud.com>
2020-09-10 16:40:46 +0800

    move file

--- include/c.h
+++ include/c.h
@@ -1,6 +1,9 @@
 #ifndef _C_H_
 #define _C_H_
 
+#pragma warning(disable : 4996)
+#pragma warning(disable : 26451)
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -10,8 +13,14 @@
 #include <memory.h>
 
 typedef size_t Size;
+
+typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
 
 /*
  * Max
