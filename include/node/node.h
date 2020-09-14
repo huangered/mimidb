59a6fb19c726aa3a23e1798c44a0802b59355f01
huangered <huangered@icloud.com>
2020-09-14 19:11:36 +0800

    add relation add method

--- include/node/node.h
+++ include/node/node.h
@@ -1,8 +1,18 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 
+<<<<<<< HEAD
 typedef struct NodeType {
     int type;
 } NodeType;
 
 #endif // !_NODE_H_
+=======
+typedef int NodeType;
+
+typedef struct CreateStmt {
+	NodeType nodetype;
+} CreateStmt;
+
+#endif
+>>>>>>> 92aa30a... add relation add method
--- src/commands/create.c
+++ src/commands/create.c
@@ -1,7 +1,13 @@
 #include "commands/create.h"
 #include "catalog/heap.h"
 #include "catalog/index.h"
+#include "commands/indexcmds.h"
 
-void DefineRelation() {
+void DefineRelation(CreateStmt* stmt) {
+	const char* name;
+	Oid id;
+	TupleDesc tupdesc;
+	heap_create(name, id, tupdesc);
 
+	DefineIndex(id, NULL);
 }
