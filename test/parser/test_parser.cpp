561116fbeb9e435efa90513071946bafd3f2e98d
peter huang <peter.huang@arm.com>
2020-09-24 17:08:26 +0800

    add configure_file

--- test/parser/test_parser.cpp
+++ test/parser/test_parser.cpp
@@ -7,6 +7,7 @@ _END_EXTERN_C
 // test the basic usage in buff mgr.
 TEST(parser, basic)
 {
-	Node* raw = raw_parse("select s from a;");
-	EXPECT_EQ(raw->nodetag, 1);
+	SelectStmt* raw = (SelectStmt*)raw_parse("select s from a;");
+	EXPECT_EQ(raw->nodetag, NT_SelectStmt);
+
 }
