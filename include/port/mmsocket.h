45d79bdf836e9afa6f323347538cf180094be0e1
huangered <huangered@icloud.com>
2020-09-25 09:40:47 +0800

    bugfix

--- include/port/mmsocket.h
+++ include/port/mmsocket.h
@@ -3,7 +3,6 @@
 
 #ifdef _WIN32
 #include <winsock2.h>
-#endif
 
 SOCKET mm_socket(int af, int type, int protocol);
 int mm_bind(SOCKET s, const struct sockaddr* name, int namelen);
@@ -12,4 +11,6 @@ SOCKET mm_accept(SOCKET s, struct sockaddr* addr, int* addrlen);
 int mm_recv(SOCKET s, char* buf, int len, int flags);
 int send(SOCKET s, const char* buf, int len, int flags);
 int mm_close(SOCKET s);
+
+#endif
 #endif
