64553c0c6d0f982f24ce730b968d322925e5eb4a
huangered <huangered@icloud.com>
2020-10-09 16:13:09 +0800

    add printf

--- src/server/server.c
+++ src/server/server.c
@@ -1,6 +1,8 @@
-#include "server/server.h"
 #include "mimi.h"
 #include "foo.h"
+#include "access/relcache.h"
+#include "server/server.h"
+#include "storage/bufmgr.h"
 #include "util/mctx.h"
 
 int
@@ -14,4 +16,10 @@ void
 Startup() {
 	printf("start up mctx");
 	MemoryContextInit();
+
+	printf("start up buffer mgr");
+	BufferInit();
+
+	printf("start relation cache");
+	RelationCacheInit();
 }
