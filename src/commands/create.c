ef727039a08d7262551f808a7799c199694d9880
huangered <huangered@icloud.com>
2020-10-12 09:25:43 +0800

    Update build warning

--- src/commands/create.c
+++ src/commands/create.c
@@ -3,18 +3,18 @@
 #include "catalog/index.h"
 #include "commands/indexcmds.h"
 
-static TupleDesc formTupleDesc(char* columns);
+static TupleDesc formTupleDesc(List* columns);
 
 void DefineRelation(CreateTableStmt* stmt) {
 	const char* name = stmt->relname;
 	Oid id = 1;
-	TupleDesc tupdesc = formTupleDesc(stmt->columns);
+	TupleDesc tupdesc = (TupleDesc)formTupleDesc(stmt->columns);
 	heap_create_with_catalog(name, id, tupdesc);
 
 	DefineIndex(id, NULL);
 }
 
 TupleDesc
-formTupleDesc(char* columns) {
+formTupleDesc(List* columns) {
 	return NULL;
 }
