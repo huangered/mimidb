afc25b1f74deda6990df4b12e785157cfaf33ba7
huangered <huangered@icloud.com>
2020-10-21 13:24:26 +0800

    add snapshot

---
+++ include/access/snapshot.h
@@ -0,0 +1,15 @@
+﻿/*
+事务snapshot相关代码
+*/
+#ifndef _SNAPSHOT_H_
+#define _SHAPSHOT_H_
+
+typedef struct SnapshotData {
+	int xmin;
+	int xmax;
+} SnapshotData;
+
+typedef SnapshotData* Snapshot;
+
+
+#endif
