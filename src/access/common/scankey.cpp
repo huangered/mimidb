f342077da44ca64cd5b59743e88d8cdda7791e34
huangered <huangered@icloud.com>
2021-04-08 15:59:32 +0800

    update scankey

--- src/access/common/scankey.cpp
+++ src/access/common/scankey.cpp
@@ -1,20 +1,15 @@
 ﻿#include "access/scankey.hpp"
-
-static FmgrInfo searchFuncByOid(Oid oid);
+#include "util/fmgr.hpp"
 /*
 * 初始化scankey
 * attrNumber	比较字段的位置
 */
 void
 ScanKeyInit(ScanKey entry, int attrNumber, StrategyNumber strategy, Datum datum, Oid funcOid) {
+	memset(entry, 0, sizeof(ScanKeyData));
+
 	entry->sk_att_no = attrNumber;
 	entry->sk_strategy = strategy;
 	entry->sk_data = datum;
-	entry->sk_func = searchFuncByOid(funcOid);
-}
-
-FmgrInfo
-searchFuncByOid(Oid oid) {
-	FmgrInfo info;
-	return info;
+	fmgr_info(funcOid, &entry->sk_func);
 }
