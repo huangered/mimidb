367605c1cb8a351c2a402c7a87b8b07515af6c57
huangered <huangered@hotmail.com>
2021-07-22 02:02:06 -0700

    add api

--- include/storage/fd.hpp
+++ include/storage/fd.hpp
@@ -1,6 +1,16 @@
 #ifndef _fd_hpp_
 #define _fd_hpp_
 
-int PathNameOpenFile(const char* filename);
+#include <sys/types.h>
+#include <dirent.h>
+
+typedef int File;
+
+File PathNameOpenFile(const char* filename);
+int FileRead(File fd, char* buf, int amount);
+int FileWrite(File fd, char* buf, int amount);
+int FileSync(File fd);
+off_t FileSeek(File fd, off_t offset, int whence);
+void FileClose(File fd);
 
 #endif
--- src/storage/smgr/fd.cpp
+++ src/storage/smgr/fd.cpp
@@ -8,15 +8,48 @@
 #include <sys/types.h>
 #endif
 
-
 int
 PathNameOpenFile(const char* filename) {
     int fd;
 #ifdef __linux__
     fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 #else
-    fd = open(filename, O_CREAT | O_RDWR , S_IREAD | S_IWRITE);
+    fd = open(filename, O_CREAT | O_RDWR, S_IREAD | S_IWRITE);
 #endif
 
     return fd;
+}
+
+int
+FileRead(File fd, char* buf, int amount) {
+    ssize_t num = read(fd, buf, amount);
+    return num;
+}
+
+int
+FileWrite(File fd, char* buf, int amount) {
+    ssize_t num = write(fd, buf, amount);
+    return num;
+}
+
+int
+FileSync(File fd) {
+#ifdef __linux__
+    return fsync(fd);
+#else
+    return _commit(fd);
+#endif
+}
+
+off_t
+FileSeek(File fd, off_t offset, int whence) {
+
+    lseek(fd, offset, whence);
+    return 0;
+}
+
+void
+FileClose(File fd) {
+    close(fd);
+    return;
 }
