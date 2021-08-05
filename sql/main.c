592c88d6c7a633ce284f258a4e965c110655be00
peter huang <peter.huang@arm.com>
2021-08-04 23:42:37 -0700

    basic sql init

--- sql/main.c
+++ sql/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int
-main(int argc, char* argv) {
-    printf("sql parser");
+main(int argc, char* argv[]) {
+    printf("sql parser %s, %s", argv[0], argv[1]);
+    char* path = argv[1];
+    char* header = argv[2];
+    char* source = argv[3];
+    
     return 0;
 }
