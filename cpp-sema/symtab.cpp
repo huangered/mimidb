ac5c55d214b450322cffe4008ae34388f23e695b
huangered <huangered@hotmail.com>
2021-08-19 23:10:24 +0800

    add log

--- cpp-sema/symtab.cpp
+++ cpp-sema/symtab.cpp
@@ -25,6 +25,9 @@ Symtab::SymbolNew(std::string name) {
         symbol->clazz = none;
         symbol->id    = nsym++;
         _data[name]   = symbol;
+#ifdef _log_	
+	printf("symbol new %s => %d\n", name.c_str(), symbol->id);
+#endif	
     }
 
     return _data[name];
@@ -42,11 +45,33 @@ void
 Symtab::Print() {
 #ifdef _log_
     printf("Symtab: %d\n", nsym);
-    printf("nterm %d\n", SymbolClass::nterm);
-    printf("token %d\n", SymbolClass::token);
+    
+    printf("nterm tag %d, num(%d)\n", SymbolClass::nterm, nnterm());
+    printf("token tag %d, num(%d)\n", SymbolClass::token, ntoken());
     for (auto it = _data.begin(); it != _data.end(); it++) {
         // How do I access each element?
         printf("  %s -> %d -> %d\n", it->first.c_str(), it->second->id, it->second->clazz);
     }
 #endif
 }
+
+int Symtab::nnterm(){
+  int i=0;
+      for (auto it = _data.begin(); it != _data.end(); it++) {
+	if(it->second->clazz == nterm) {
+	  i++;
+	}
+      }
+  return i;
+}
+
+int Symtab::ntoken(){
+  int i=0;
+      for (auto it = _data.begin(); it != _data.end(); it++) {
+	if(it->second->clazz == token) {
+          i++;
+	}
+      }
+  return i;
+}
+
