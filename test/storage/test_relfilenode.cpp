d300d5b7c8c35bc034972b13e059697eb7dda562
peter huang <peter.huang@arm.com>
2021-08-29 22:31:19 -0700

    release mison

--- test/storage/test_relfilenode.cpp
+++
@@ -1,8 +0,0 @@
-#include "../g.hpp"
-#include "storage/relfilenode.hpp"
-
-TEST(relfilenode, basic) {
-    RelFileNode a{ 1, 2 }, b{ 1, 2 }, c{ 2, 3 };
-    EXPECT_EQ(a, b);
-    EXPECT_NE(a, c);
-}
