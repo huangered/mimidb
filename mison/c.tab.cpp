253702416883b86cce82c7eb97a69b7e6cdbc496
peter huang <peter.huang@arm.com>
2021-09-06 18:17:02 -0700

    update

--- mison/c.tab.cpp
+++ mison/c.tab.cpp
@@ -2203,8 +2203,6 @@ Parser::yyshift(std::stack<int>& states, std::stack<YYSTYPE>& syms, std::stack<I
     int curStateId    = states.top();
     InputToken* token = input.top();
 
-    printf("debug (state, tok) (%d, %d)\n", curStateId, token->tok);
-
     int rd       = action_table[curStateId][token->tok];
     *acc         = (rd == 10000);
     bool r_state = (rd > 0);
