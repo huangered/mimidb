2bddd2117d1c9d8f16e754fdd768ff011ac6e2f4
huangered <huangered@hotmail.com>
2021-10-09 22:15:25 +0800

    update test

--- unittest/storage/test_fsm.cpp
+++ unittest/storage/test_fsm.cpp
@@ -1,15 +1,8 @@
 #include "../g.hpp"
 
-#include "storage/bufmgr.hpp"
-#include "storage/fsm_internal.hpp"
 #include "storage/freespace.hpp"
-#include "storage/smgr.hpp"
 
-TEST(fsm, print) {
-    printf("%d\n%d\n%d\n", NodesPerPage, NonLeafNodesPerPage, LeafNodesPerPage);
-}
-
-TEST(fsm, leaf) {
+TEST(fsm, test_record) {
     Relation rel = new RelationData{};
     rel->rd_id   = 2000;
     rel->rd_node = { 2000, rel->rd_id };
